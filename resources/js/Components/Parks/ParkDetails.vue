23ee469fafc78c8c86be1e92cc19e56d4cb3f507
Modem-i4 <vasea.tshuh@gmail.com>
2025-12-19 21:03:05 +0000

    Feat(ParksDetails): Show image picker for editors by default

--- resources/js/Components/Parks/ParkDetails.vue
+++ resources/js/Components/Parks/ParkDetails.vue
@@ -120,6 +120,7 @@ function pickerSaved(newImages) {
 
     <ImageSlider :modelId="parkStore.selectedMarker?.id || null" model="parks" class="my-2" ref="imageSliderRef"
       :editable="authStore.can.upload"
+      :showByDefault="!isMobile || authStore.can.upload"
       @onImageClick="() => { if(authStore.can.upload) startGalleryChange() }"
       @close="closeImagePicker"/>
 
