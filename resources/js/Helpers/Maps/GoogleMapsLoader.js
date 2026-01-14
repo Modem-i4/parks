d0c401bea3a435865232654f87b3dfc01e24d51e
Modem-i4 <vasea.tshuh@gmail.com>
2026-01-14 17:52:41 +0000

    Chore(Maps): Set default locale to uk

--- resources/js/Helpers/Maps/GoogleMapsLoader.js
+++ resources/js/Helpers/Maps/GoogleMapsLoader.js
@@ -3,7 +3,9 @@ import { Loader } from '@googlemaps/js-api-loader'
 
 const loader = new Loader({
   apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  version: 'beta',
+  language: "uk",
+  region: "UA",
+  version: 'weekly',
   libraries: ['marker'],
 })
 
